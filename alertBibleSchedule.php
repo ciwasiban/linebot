536652866a7259c57c4cccc3f399da7255df30dd
FionYeh <fionyeh@kkbox.com.tw>
2019-02-26 23:51:11 +0800

    Add a new tool that can alert bible chart to the people in line room everyday

---
+++ alertBibleSchedule.php
@@ -0,0 +1,16 @@
+<?php
+require_once(__DIR__ . '/myconfig.php');
+require_once(__DIR__ . '/bibleSchedule.php');
+// get this date
+$getdate = getdate();
+$thisDate = sprintf('%s/%s', $getdate['mon'], $getdate['mday']);
+
+// Get Today's Bible Chart
+$todayBibleChart = $bibleList[$thisDate];
+
+// Body
+$content = sprintf('%s %s', '今日', $todayBibleChart);
+define('CONTENT', $content);
+
+//define ('GROUP_ID', $groupIdLdtJoyce);
+require_once( __DIR__ . '/saysomething.php');
