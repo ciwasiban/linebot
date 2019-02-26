cdfe46863ae5f0eb1c9969eb7309508740743e7d
FionYeh <fionyeh@kkbox.com.tw>
2019-02-27 01:52:28 +0800

    Fix style

--- alertBibleChapter.php
+++ alertBibleChapter.php
@@ -12,7 +12,7 @@ $todayBibleChart = $bibleList[$thisDate];
 
 // Body
 $content = sprintf('%s %s', '今日', $todayBibleChart);
-define('CONTENT', $content);
+define ('CONTENT', $content);
 
 //define ('GROUP_ID', $groupIdReadBible);
 require_once( __DIR__ . '/saysomething.php');
