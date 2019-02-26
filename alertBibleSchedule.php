c2a17aeb939a2ac4e80fca7a6b1d0be6f2781853
FionYeh <fionyeh@kkbox.com.tw>
2019-02-27 00:54:33 +0800

    Add timezone

--- alertBibleSchedule.php
+++ alertBibleSchedule.php
@@ -1,7 +1,9 @@
 <?php
 require_once(__DIR__ . '/myconfig.php');
 require_once(__DIR__ . '/bibleSchedule.php');
+
 // get this date
+date_default_timezone_set('Asia/Taipei');
 $getdate = getdate();
 $thisDate = sprintf('%s/%s', $getdate['mon'], $getdate['mday']);
 
@@ -12,5 +14,5 @@ $todayBibleChart = $bibleList[$thisDate];
 $content = sprintf('%s %s', '今日', $todayBibleChart);
 define('CONTENT', $content);
 
-//define ('GROUP_ID', $groupIdLdtJoyce);
+//define ('GROUP_ID', $groupIdReadBible);
 require_once( __DIR__ . '/saysomething.php');
