12491107bf53ed4a50dff3b5d270c03a63cc2234
FionYeh <fionyeh@kkbox.com.tw>
2019-02-27 20:45:43 +0800

    Alert for Joyce's Ldt group.

---
+++ alertLdtJoyce.php
@@ -0,0 +1,9 @@
+<?php
+require_once(__DIR__ . '/myconfig.php');
+
+// Body
+$content = sprintf('%s', '明天九點門訓大家都ok嗎？');
+define ('CONTENT', $content);
+
+//define ('GROUP_ID', $groupIdLdtJoyce);
+require_once( __DIR__ . '/saysomething.php');
