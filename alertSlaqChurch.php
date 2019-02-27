adc59f83763b0eada70d40c922ec178ac64de9e5
FionYeh <fionyeh@kkbox.com.tw>
2019-02-27 20:46:36 +0800

    Modify content

--- alertSlaqChurch.php
+++ alertSlaqChurch.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/myconfig.php');
 
 // Body
-$content = sprintf('%s', '請協助回報本週聚會人數及明天獻詩');
+$content = sprintf('%s', '請協助回報本週聚會人數及明天獻詩,感謝~');
 define ('CONTENT', $content);
 
 //define ('GROUP_ID', $groupIdSlaq);
