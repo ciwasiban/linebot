2be6b8d0d1b397b25d9fb945460bf1f3933be5c8
FionYeh <fionyeh@kkbox.com.tw>
2019-02-27 20:37:06 +0800

    Alert for slaq church

---
+++ alertSlaqChurch.php
@@ -0,0 +1,9 @@
+<?php
+require_once(__DIR__ . '/myconfig.php');
+
+// Body
+$content = sprintf('%s', '請協助回報本週聚會人數及明天獻詩');
+define ('CONTENT', $content);
+
+//define ('GROUP_ID', $groupIdSlaq);
+require_once( __DIR__ . '/saysomething.php');
