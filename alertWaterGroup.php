8f50c9a1c540477d5069faadab432274773a7bac
FionYeh <fionyeh@kkbox.com.tw>
2019-03-21 23:30:01 +0800

    Modify wording

--- alertWaterGroup.php
+++ alertWaterGroup.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/myconfig.php');
 
 // Body
-$content = sprintf("%s\n\n%s", '這週日下午要一起拉水管，不能到的請提早請假，以便安排人手，感謝~', '(補充說明：會議紀錄提到每戶至少要出一人，未到的要繳交500元，用作當天我們另外請工人之薪資)');
+$content = sprintf("%s\n\n%s", '這週日下午 13:00 在二伯家集合，一起出發拉水管，不能到的請提早請假，以便安排人手，感謝~', '(補充說明：會議紀錄提到每戶至少要出一人，未到的要繳交500元，用作當天我們另外請工人之薪資)');
 define ('CONTENT', $content);
 
 define ('GROUP_ID', $groupIdWater);
