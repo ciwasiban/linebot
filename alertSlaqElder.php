2053c4670d4d3de4c64b5cf4f631d47e172eb0e2
FionYeh <fionyeh@kkbox.com.tw>
2019-03-28 22:32:33 +0800

    Add it for alert to slaq elder's room

---
+++ alertSlaqElder.php
@@ -0,0 +1,27 @@
+<?php
+require_once(__DIR__ . '/myconfig.php');
+
+
+// get command line argv
+unset($argv1);
+if ( 1 < $argc) {
+    $program = array_shift($argv);
+    $argv1 = array_shift($argv);
+}
+
+// Body
+switch ($argv1) {
+    case 'photo':
+        $content = sprintf('%s', '月底到了，麻煩凱悅每月最後一天整理 Line 相簿備份到電腦,感謝~');
+        break;
+    default:
+        // do nothing
+
+}
+
+// Alert
+if (isset($content)) {
+    define ('CONTENT', $content);
+    define ('GROUP_ID', $groupIdLdtSlaqElder);
+    require_once( __DIR__ . '/saysomething.php');
+}
