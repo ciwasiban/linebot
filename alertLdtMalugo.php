b759d25a8a7f4f36fb2425a3c167b7db1b8735d7
ciwasiban <apple@Fions-MBP.local>
2019-05-12 23:53:43 +0800

    Modify time.

--- alertLdtMalugo.php
+++ alertLdtMalugo.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/myconfig.php');
 
 // Body
-$content = sprintf('%s', '明天 10:30 小組大家都ok嗎？');
+$content = sprintf('%s', '明天 10:00 小組大家都ok嗎？');
 define ('CONTENT', $content);
 
 define ('GROUP_ID', $groupIdLdtMalugo);
