6c71bb43b4e418843d75361ac4c73e89c1a2396e
ciwasiban <ciwasiban@gmail.com>
2019-06-28 22:56:01 +0800

    Modify time.

--- alertLdtJing.php
+++ alertLdtJing.php
@@ -2,7 +2,7 @@
 require_once(__DIR__ . '/myconfig.php');
 
 // Body
-$content = sprintf('%s', '明天(四)上午 11:40 門訓，美菁姐 ok 嗎？');
+$content = sprintf('%s', '明天(四)上午 10:40 門訓，美菁姐 ok 嗎？');
 define ('CONTENT', $content);
 
 define ('GROUP_ID', $groupIdLdtJing);
