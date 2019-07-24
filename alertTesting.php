2c251ab803f31f4aa3070e1e50a3c3ec7d8e2e0f
ciwasiban <ciwasiban@gmail.com>
2019-07-24 18:35:48 +0800

    Testing for bible bot.

--- alertTesting.php
+++ alertTesting.php
@@ -1,11 +1,14 @@
 <?php
 require_once(__DIR__ . '/myconfig.php');
 
+define("CHANNEL_ACCESS_TOKEN", $channelAccessTokenOfBible);
+define("CHANNEL_SECRET", $channelSecretOfBible);
+
 // Body
 date_default_timezone_set('Asia/Taipei');
 $time = date("Y-m-d H:i:s");
 $content = sprintf("%s\n%s", 'Just Testing', $time);
 define ('CONTENT', $content);
 
-define ('GROUP_ID', $groupIdHey);
+define ('GROUP_ID', $groupIdHeyOfBible);
 require_once( __DIR__ . '/saysomething.php');
