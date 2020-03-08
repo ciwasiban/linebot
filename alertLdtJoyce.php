5e1dfcdb1c5c8c4ef99fb0191c3d8729925a74df
ciwasiban <ciwasiban@gmail.com>
2020-03-08 23:04:08 +0800

    Renew alert date.

--- alertLdtJoyce.php
+++ alertLdtJoyce.php
@@ -5,7 +5,7 @@ define("CHANNEL_ACCESS_TOKEN", $ldtChannelAccessToken);
 define("CHANNEL_SECRET", $ldtChannelSecret);
 
 // Body
-$content = sprintf('%s', '明天(六)早上 10:00  門訓大家都ok嗎？');
+$content = sprintf('%s', '明天(三)晚上 22:30  門訓大家都ok嗎？');
 define ('CONTENT', $content);
 
 define ('GROUP_ID', $groupIdLdtJoyce);
